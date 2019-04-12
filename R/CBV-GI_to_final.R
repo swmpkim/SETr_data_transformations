53ed8e211b87703a904c40a465f49718b4db3e57
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-04-12 09:06:26 -0500

    CBV-GI fixed errors and full transform

--- R/CBV-GI_to_final.R
+++ R/CBV-GI_to_final.R
@@ -6,7 +6,9 @@ library(unpivotr)
 library(janitor)
 library(here)
 
-path <- here::here("data", "submitted", "2019-04-11_CBV_GI.xlsx")
+path <- here::here("data", "submitted", "2019-04-12_CBV_GI.xlsx")
+# there were some copy-and-paste issues in the originally submitted file
+# they have been corrected in 2019-04-12 file
 
 # looks like I can loop through sheets
 
@@ -23,7 +25,8 @@ dat_all <- reshape::merge_recurse(dat) %>%
 unique(dat_all$code)
 
 dat_all_qc <- dat_all %>%
-        mutate(arm_qaqc_code = NA_character_,
+        mutate(arm_qaqc_code = case_when(!is.na(notes) ~ notes,
+                                         TRUE ~ NA_character_),
                pin_qaqc_code = case_when(code == "Shell" ~ "HSM SH",
                                          code == "Hole" ~ "LHE",
                                          code == "Mound" ~ "HMD",
@@ -31,6 +34,51 @@ dat_all_qc <- dat_all %>%
                                          code == "Burrow" ~ "LHE CB",
                                          code == "Plant Root" ~ "HPL RT",
                                          code == "Root" ~ "HPL RT",
-                                         code == "Clump" ~ "HPL",
-                                         code == "Plant Mound" ~ "HPL",
-                                         TRUE ~ NA_character_))
+                                         code == "Clump" ~ "HPL RT",
+                                         code == "Plant Mound" ~ "HPL RT",
+                                         TRUE ~ NA_character_),
+               date = as.Date(date),
+               pin_number = paste0("pin_", pin_number)) %>%
+        select(reserve, set_id, date, arm_position = arm_positon, arm_qaqc_code,
+               pin_number, height_mm = pin_height_mm, qaqc_code = pin_qaqc_code)
+# per email from Scott Lerberg on 4/11/19 - clump and plant mound at this site "can probably be classified as plant root, HPL RT".
+
+dupes <- get_dupes(dat_all_qc, set_id, date, arm_position, pin_number)
+dupes_5_2 <- get_dupes(dat[[10]] %>% clean_names(), set_id, date, arm_positon, pin_number)
+# two sets of 2012-03-20 readings at SET 5-2
+# Second one is 9-13 and has been changed
+# also in the original spreadsheet the set_id in sheet 4-2 was
+# mistakenly pasted as 4-1
+# so the file 2019-04-12 IS THE CORRECTED FILE
+
+
+
+
+get_dupes(dat_all_qc, set_id, date, arm_position, pin_number)
+
+
+spec <- dat_all_qc %>%
+        expand(pin_number, .value = c("height_mm", "qaqc_code")) %>%
+        mutate(.name = paste0(pin_number, "_", .value))
+
+dat_wide <- dat_all_qc %>%
+        select(-arm_qaqc_code) %>%
+        filter(set_id != "GI 5-2") %>%
+        pivot_wider(spec = spec) %>%
+        select(set_id, date, arm_position, 
+               pin_1_height_mm, pin_2_height_mm, pin_3_height_mm, 
+               pin_4_height_mm, pin_5_height_mm, pin_6_height_mm, 
+               pin_7_height_mm, pin_8_height_mm, pin_9_height_mm,
+               pin_1_qaqc_code, pin_2_qaqc_code, pin_3_qaqc_code, 
+               pin_4_qaqc_code, pin_5_qaqc_code, pin_6_qaqc_code, 
+               pin_7_qaqc_code, pin_8_qaqc_code, pin_9_qaqc_code,
+               everything()) 
+
+dupes <- get_dupes(dat_wide, set_id, date, arm_position)
+
+
+# create the file path
+xlpath <- here::here("data", "final", "cbv_giset.xlsx")
+
+# source the script that generates the excel file
+source(here::here("R", "excel_sheet_script.R"))
