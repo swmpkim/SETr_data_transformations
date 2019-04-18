a3d7915108b70fc5a2fbac01f649fcd2a615203a
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-04-18 10:25:28 -0500

    NAR - Nag imported

--- R/NAR_to_intermediate.R
+++ R/NAR_to_intermediate.R
@@ -16,33 +16,35 @@ path <- here::here("data", "submitted", "2019-04-18_NAR.xlsx")
 # tidyxl/unpivotr book: https://nacnudus.github.io/spreadsheet-munging-strategies/small-multiples-with-all-headers-present-for-each-multiple.html
 
 
+##############################################################################
+# background in case it's needed
+##############################################################################
 
-all_cells <- xlsx_cells(path, sheets = "Cogg data") 
-
-
-head(all_cells)
-
+# head(cog_cells)
 
 
 # only take that first table and figure out how to deal with it
-table1 <- all_cells %>% 
-        filter(row %in% 2:13,
-               col %in% 1:5) %>% 
-        select(row, col, data_type, character, numeric, local_format_id)
-rectify(table1)
-
-t1_tidy <- table1 %>% 
-        behead("NNW", set_date) %>% 
-        behead("N", arm_position) %>% 
-        behead("N", arm_bearing) %>% 
-        behead("W", pin_number) %>% 
-        filter(!is.na(pin_number)) %>% 
-        select(set_date, arm_position, arm_bearing, pin_number,
-               height_mm = numeric) %>%
-        arrange(arm_position, pin_number) 
+# table1 <- all_cells %>% 
+#         filter(row %in% 2:13,
+#                col %in% 1:5) %>% 
+#         select(row, col, data_type, character, numeric, local_format_id)
+# rectify(table1)
+# 
+# t1_tidy <- table1 %>% 
+#         behead("NNW", set_date) %>% 
+#         behead("N", arm_position) %>% 
+#         behead("N", arm_bearing) %>% 
+#         behead("W", pin_number) %>% 
+#         filter(!is.na(pin_number)) %>% 
+#         select(set_date, arm_position, arm_bearing, pin_number,
+#                height_mm = numeric) %>%
+#         arrange(arm_position, pin_number) 
 # that's in a pretty good long format
 
-# make it a function
+
+###############################################################################
+# make a function to unpivot a batch of cells
+###############################################################################
 
 unpivot <- function(cells){
         cells %>% 
@@ -58,19 +60,51 @@ unpivot <- function(cells){
 }
 
 
+################################################################################
+# work with Cogg
+################################################################################
+
+cog_cells <- xlsx_cells(path, sheets = "Cogg data") 
 
 # the anchoring corner of the cells, in the Cogg sheet,
 # is cells that start with "Cog"
-corners <- 
-        all_cells %>% 
+cog_corners <- 
+        cog_cells %>% 
         filter(str_detect(character, "^Cog")) %>% 
         select(row, col)
 
 # partition the cells
-partitions <- partition(all_cells, corners)
+cog_partitions <- partition(cog_cells, cog_corners)
 
 
-Cog_tidy <- partitions %>%
+cog_tidy <- cog_partitions %>%
         mutate(cells = map(cells, unpivot)) %>%
         unnest() %>%
-        select(-corner_row, -corner_col) 
+        select(-corner_row, -corner_col) %>% 
+        filter(!is.na(height_mm))  # only doing this because the only NAs happen when the whole SET is missing or a comment got read in from a weird place
+
+
+###############################################################################
+# work with Nag
+###############################################################################
+
+nag_cells <- xlsx_cells(path, sheets = "Nag data") 
+
+# the anchoring corner of the cells, in the nagg sheet,
+# is cells that start with "nag"
+nag_corners <- 
+        nag_cells %>% 
+        filter(str_detect(character, "^Nag")) %>% 
+        select(row, col)
+
+# partition the cells
+nag_partitions <- partition(nag_cells, nag_corners)
+
+
+nag_tidy <- nag_partitions %>%
+        mutate(cells = map(cells, unpivot)) %>%
+        unnest() %>%
+        select(-corner_row, -corner_col) %>% 
+        filter(!is.na(height_mm)) 
+
+
