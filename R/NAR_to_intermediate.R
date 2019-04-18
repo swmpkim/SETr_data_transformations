a9e855152a8f64754548bd78e869d9b0ef66460b
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-04-18 10:56:06 -0500

    NAR finished and exported!

--- R/NAR_to_intermediate.R
+++ R/NAR_to_intermediate.R
@@ -10,6 +10,9 @@ path <- here::here("data", "submitted", "2019-04-18_NAR.xlsx")
 # in the 4-18 xlsx file, I've added the dates that Kenny sent
 # of Cogg sampling in 2014 and 2015
 # (they were missing in original)
+# also copied 10-26-16 down into Cog 11 and 12
+# because they didn't have dates
+# in Nag, Nag 3+ had 2013 dates in 2014. So I copied 10.21.2014 from Nag 1/2
 
 
 # following along with the small multiples chapter of the 
@@ -108,3 +111,49 @@ nag_tidy <- nag_partitions %>%
         filter(!is.na(height_mm)) 
 
 
+###############################################################################
+# combine
+###############################################################################
+dat_all <- bind_rows(cog_tidy, nag_tidy)
+
+# separate out set and date
+# get date into the right format
+# insert qaqc columns
+dat_long <- dat_all %>% 
+        separate(set_date, into = c("site", "set_num", "date"), sep = " ") %>% 
+        mutate(set_id = paste(site, set_num),
+               date = lubridate::mdy(date),
+               pin_number = tolower(pin_number),
+               pin_number = gsub(" ", "_", pin_number),
+               qaqc_code = NA_character_,
+               arm_qaqc_code = NA_character_) %>% 
+        select(-site, -set_num) %>% 
+        select(set_id, everything()) 
+
+# check for dupes
+janitor::get_dupes(dat_long, set_id, date, arm_position, pin_number)
+
+
+# pivot to wide
+spec <- dat_long %>%
+        expand(pin_number, .value = c("height_mm", "qaqc_code")) %>%
+        mutate(.name = paste0(pin_number, "_", .value))
+
+dat_wide <- dat_long %>%
+        pivot_wider(spec = spec) %>%
+        select(set_id, date, arm_position, arm_bearing, arm_qaqc_code,
+               pin_1_height_mm, pin_2_height_mm, pin_3_height_mm, 
+               pin_4_height_mm, pin_5_height_mm, pin_6_height_mm, 
+               pin_7_height_mm, pin_8_height_mm, pin_9_height_mm,
+               pin_1_qaqc_code, pin_2_qaqc_code, pin_3_qaqc_code, 
+               pin_4_qaqc_code, pin_5_qaqc_code, pin_6_qaqc_code, 
+               pin_7_qaqc_code, pin_8_qaqc_code, pin_9_qaqc_code,
+               everything()) 
+
+
+# export
+# create the file path
+xlpath <- here::here("data", "final", "narset.xlsx")
+
+# source the script that generates the excel file
+source(here::here("R", "excel_sheet_script.R"))
