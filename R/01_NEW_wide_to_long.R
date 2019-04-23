ff8f7a6102f8145c3eed7d6fa4be5da4627a7b97
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-04-23 09:11:54 -0500

    progress on pivoting

--- R/01_NEW_wide_to_long.R
+++ R/01_NEW_wide_to_long.R
@@ -40,5 +40,50 @@ dat_formatted <- dat %>%
 
 # have to pivot to longer first, then bust up the column names
 
+# have to change column names first because there are too many underscores
+names(dat_formatted) <- gsub("qaqc_code", "qaqccode", names(dat_formatted))
+names(dat_formatted) <- gsub("pin_", "pin", names(dat_formatted))
+names(dat_formatted) <- gsub("height_", "height", names(dat_formatted))
 
+
+spec <- dat_formatted %>% 
+        pivot_longer_spec(
+                cols = starts_with("pin1_height"):"pin9_qaqccode",
+                names_to = c("pinnum", ".value"),
+                names_sep = "_"
+        )
+
+
+dat_long <- dat_formatted %>% 
+        pivot_longer(spec = spec)
+
+
+
+###############################################################################
+# this is how it was set up to pivot to wide:
+spec <- dat %>%
+        expand(pin_number, .value = c("height_cm", "qaqc_code")) %>%
+        mutate(.name = paste0(pin_number, "_", .value))
+
+
+
+
+###############################################################################
+# this is kind of how i did it in earlier scripts:
+
+# find the columns that contain mm or cm in their names
+pos_of_hts <- grep("mm|cm", names(dat_formatted))
+# find the columns with 'flag' in their names
+pos_of_flags <- grep("pin_[c(1,2,3,4,5,6,7,8,9)]_qaqc_code", names(dat_formatted))
+# pull all pin heights and flags into long format,
+# then separate column names to identify pin numbers and type of information (flag or height)
+# and spread back out so there's one row per pin, and a column each for height and flag
+dat_long <- dat_formatted %>%
+        gather(key = "param", value = "value", c(pos_of_flags, pos_of_hts)) %>%
+        separate(param, into = c("pin", "num", "type", "units")) %>%
+        mutate(pin_number = paste(pin, num, sep = "_")) %>%
+        select(-pin, -num) %>%
+        mutate(type = case_when(type == "code" ~ "code",
+                                TRUE ~ paste0("pin_height_", type))) %>%
+        spread(key = type, value = value)
         
